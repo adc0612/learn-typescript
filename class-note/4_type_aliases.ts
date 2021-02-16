4cd9bc22495f1abb40b5eb196f421b4416ff60cb
Dohu <adc0612@gmail.com>
2021-02-16 19:52:27 +0900

    type-aliases upload

---
+++ class-note/4_type_aliases.ts
@@ -0,0 +1,18 @@
+// MyString타입은 string을 뜻함 
+type MyString = string;
+let str:MyString = 'sdsdsd';
+
+type TodoObj = {id: string; title: string; done: boolean};
+function getTodo(todo:TodoObj) {
+    
+}
+
+type PersonType = {
+    name: string;
+    age: number;
+}
+
+let sejun:PersonType = {
+    name: 'sejun',
+    age: 23,
+}
